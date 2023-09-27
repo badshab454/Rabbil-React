2390b61b8094b06355f2118c73ab2197b526bed2
Badsha Hossain <badshab454@gmail.com>
2023-09-27 12:16:53 +0600

    if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Demo from "./component/Demo";
-
 const App = () => {
+  let marks = 85;
   return (
     <div>
-      <h1>Im learning React.</h1>
-      <Demo />
+      {(()=>{
+        if(marks >= 80) {
+          return "A+";
+        } else if (marks >= 70) {
+          return "A";
+        } else if (marks >= 60) {
+          return "A-";
+        } else if ( marks >= 50) {
+          return "B";
+        } else if (marks >=  40) {
+          return "C";
+        } else if (marks >= 33) {
+          return "D";
+        } else {
+          return "F";
+        }
+      })()}          
     </div>
   );
 };
