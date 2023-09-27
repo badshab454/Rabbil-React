87939cbb7beb653020abaa1ab7805f04ecb25470
Badsha Hossain <badshab454@gmail.com>
2023-09-27 12:24:44 +0600

    immediately-invoked

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 const App = () => {
-  let marks = 85;
+  let marks = 56;
   return (
     <div>
       {(()=>{
-        if(marks >= 80) {
+        if(marks >= 80 && marks <= 100) {
           return "A+";
-        } else if (marks >= 70) {
+        } else if (marks >= 70 && marks < 80) {
           return "A";
-        } else if (marks >= 60) {
+        } else if (marks >= 60 && marks < 70) {
           return "A-";
-        } else if ( marks >= 50) {
+        } else if ( marks >= 50 && marks < 60) {
           return "B";
-        } else if (marks >=  40) {
+        } else if (marks >=  40 && marks < 50) {
           return "C";
-        } else if (marks >= 33) {
+        } else if (marks >= 33 && marks < 40) {
           return "D";
         } else {
           return "F";
