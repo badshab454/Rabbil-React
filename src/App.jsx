d17f2ef11fc5792add784ac6a63acdf472c691b4
Badsha Hossain <badshab454@gmail.com>
2023-09-29 16:32:18 +0600

    map method

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 const App = () => {
-  let marks = 56;
+  const city = ['Dhaka','London','Delhi','Kolkata'];
   return (
     <div>
-      {(()=>{
-        if(marks >= 80 && marks <= 100) {
-          return "A+";
-        } else if (marks >= 70 && marks < 80) {
-          return "A";
-        } else if (marks >= 60 && marks < 70) {
-          return "A-";
-        } else if ( marks >= 50 && marks < 60) {
-          return "B";
-        } else if (marks >=  40 && marks < 50) {
-          return "C";
-        } else if (marks >= 33 && marks < 40) {
-          return "D";
-        } else {
-          return "F";
+      <ol>
+        {
+          city.map((item,i)=>{
+            return <li key={i.toString()}>{item}</li>
+          })
         }
-      })()}          
+      </ol>
+           
     </div>
   );
 };
