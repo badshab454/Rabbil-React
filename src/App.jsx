3391e65bb577c91f7e8a8312c1b345fe367c47e1
Badsha Hossain <badshab454@gmail.com>
2023-10-07 13:02:13 +0600

    bootstrap installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import {useRef} from "react";
-
 const App = () => {
-  let myHeadLine=useRef();
-  const change = () => {
-    myHeadLine.current.innerHTML="<ul><li>A</li><li>B</li></ul>";
-                                      
-  }
   return (
     <div>
-      <h1 ref={myHeadLine}></h1>
-      <button onClick={change}>Click Me</button>
+      
     </div>
-  ); 
+  );
 };
 
 export default App;
