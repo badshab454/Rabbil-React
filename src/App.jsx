cf1b71959ff0257410d885acd7af36b74e20b493
Badsha Hossain <badshab454@gmail.com>
2023-10-08 12:50:16 +0600

    useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import {useState} from 'react';
 
 const App = () => {
+  const [number, setNumber] = useState(0);
   return (
     <div>
-      <h1 className="text-success">text success</h1>
+      <h1>Number clicked {number} times.</h1>
+      <button onClick={() => setNumber(number + 1)}>Clicked</button>
     </div>
   );
 };
